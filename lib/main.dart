5b31e340ed64b03732921cfa944091ec41ddd99d
cristianoDev <cristianolourenco319@gmail.com>
2025-07-11 15:01:48 +0100

    configuring firebase cli

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:chat/firebase_options.dart';
 import 'package:chat/screens/auth_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
 
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -8,10 +8,23 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  bool _isLogin = true;
+
+  final _formKey = GlobalKey<FormState>();
+  String _enteredPassword = "";
+  String _enteredEmail = "";
+
+  void _submit() {
+    final isValid = _formKey.currentState!.validate();
+    if (isValid) {
+      _formKey.currentState!.save();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.primary,
+      backgroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
       body: Center(
         child: SingleChildScrollView(
           child: Column(
@@ -34,6 +47,7 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Padding(
                     padding: EdgeInsets.all(16),
                     child: Form(
+                      key: _formKey,
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
@@ -41,14 +55,59 @@ class _AuthScreenState extends State<AuthScreen> {
                             keyboardType: TextInputType.emailAddress,
                             autocorrect: false,
                             textCapitalization: TextCapitalization.none,
+                            onSaved: (newValue) {
+                              _enteredEmail = newValue ?? "";
+                            },
                             decoration: InputDecoration(
                               labelText: "Email Address",
                             ),
+
+                            validator: (val) {
+                              if (val == null ||
+                                  val.trim().isEmpty ||
+                                  !val.contains("@")) {
+                                return "Please enter a valid email address";
+                              }
+                              return null;
+                            },
                           ),
                           TextFormField(
                             keyboardType: TextInputType.visiblePassword,
                             obscureText: true,
+                            onSaved: (newValue) {
+                              _enteredEmail = newValue ?? "";
+                            },
                             decoration: InputDecoration(labelText: "Password"),
+
+                            validator: (val) {
+                              if (val == null || val.trim().length < 6) {
+                                return "Password must be at laest 6 characters long";
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(height: 12),
+                          ElevatedButton(
+                            onPressed: _submit,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Theme.of(
+                                    context,
+                                  ).colorScheme.primaryContainer,
+                            ),
+                            child: Text(_isLogin ? 'Login' : "Signup"),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            child: Text(
+                              _isLogin
+                                  ? "Create an account"
+                                  : "I already have an account",
+                            ),
                           ),
                         ],
                       ),
